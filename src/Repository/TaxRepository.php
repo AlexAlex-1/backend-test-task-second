882a638801a1ccf4c6a4f9b89986a9bb3500ea59
AlexAlex-1 <askorov00@gmail.com>
2024-09-26 22:07:52 +0200

    Add the Tax Entity

---
+++ src/Repository/TaxRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Tax;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Tax>
+ */
+class TaxRepository extends ServiceEntityRepository
+{
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tax::class);
+    }
+}
